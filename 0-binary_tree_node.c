031160728b55985c18e54808d9cc2fef41256dce
Aadane <adanemoges6@gmail.com>
2023-08-30 13:17:29 +0300

    all mandatory files are created

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/***
+ * binary_tree_node - function namekjh;oihkn.lkm,
+ * @parent: the variable
+ * @value: the node value
+ * Returns: pointer to the root node
+ *
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (!node)
+		return (NULL);
+}
