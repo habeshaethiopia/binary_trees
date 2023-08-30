309fc04cb76590ce53637e84f457bd530bdeb21b
Aadane <adanemoges6@gmail.com>
2023-08-30 15:31:19 +0300

    THE RECORECTING STAF

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,6 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	Node->n = value;
 	Node->right = NULL;
+	Node->left = NULL;
 	Node->parent = parent;
 	if (parent->left)
 	{
