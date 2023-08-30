d99a1c16d42f417285f6d831e0b1cd337ce34fd9
Aadane <adanemoges6@gmail.com>
2023-08-30 13:28:42 +0300

    all mandatory files are created

--- binary_trees.h
+++ binary_trees.h
@@ -13,10 +13,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
