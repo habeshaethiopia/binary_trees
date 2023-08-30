799d4b43373f7f59835e977a7cb45f041dd5d13a
Aadane <adanemoges6@gmail.com>
2023-08-30 16:25:35 +0300

    my try

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,36 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_delete- func to delete the binary tree
+ *
+ * @tree: the root of the tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *temp;
+	binary_tree_t *current = tree;
+	binary_tree_t *parent = NULL;
+
+	while (current)
+	{
+		if (current->left)
+			current->left = current;
+		else if (current->right)
+			current = current->right;
+		else
+			parent = current->parent;
+		{
+			if (parent)
+			{
+				if (parent->left == current)
+					parent->left = NULL;
+				else
+					parent->right = NULL;
+			}
+			temp = current;
+			current = parent;
+			free(temp);
+		}
+	}
+	if (tree)
+		free(tree);
+}
