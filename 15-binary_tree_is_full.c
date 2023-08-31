7c4b2bd7a233b105561e9a0f78f5b0cae2f51943
Aadane <adanemoges6@gmail.com>
2023-08-31 14:19:22 +0300

    is full?

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,1 +1,33 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_is_full -is full
+ *
+ * @tree: the parameter
+ * Return: 0 or 1
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	size_t full;
+
+	if (!tree)
+		return (0);
+	full = have_place(tree);
+	return (full > 0 ? 0 : 1);
+}
+/**
+ * have_place - check if aplace for a child
+ *
+ * @tree: the root
+ * Return: size_t
+ */
+size_t have_place(const binary_tree_t *tree)
+{
+	size_t place;
+
+	if (!tree)
+		return (0);
+	place = have_place(tree->left);
+	place += have_place(tree->right);
+	return (!((tree->left && tree->right) || /
+	(!tree->left && !tree->right)) ? place + 1 : place);
+}
