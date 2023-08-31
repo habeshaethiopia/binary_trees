246ef5408bbe5e2f5ed358f2be906356b2d3d78a
Aadane <adanemoges6@gmail.com>
2023-08-31 11:51:28 +0300

    inorder traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_inorder - traversal function
+ *
+ * @tree:the variables
+ * @func:the variables
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_preorder(tree->left, func);
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+}
