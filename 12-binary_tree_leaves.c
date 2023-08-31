016a98198473b136beda690745ce153efc3dc023
Aadane <adanemoges6@gmail.com>
2023-08-31 13:12:11 +0300

    leaves of the tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,18 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_leaves - counts the leafs
+ *
+ * @tree: the root tree
+ * Return: number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves;
+
+	if (!tree)
+		return (0);
+	leaves = binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+
+	return ((!tree->left && !tree->right) ? leaves + 1 : leaves);
+}
