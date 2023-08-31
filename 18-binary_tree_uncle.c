c68d92140cf4c810ab20738153562db26d5d808b
Aadane <adanemoges6@gmail.com>
2023-08-31 15:12:29 +0300

    the uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_uncle - find the uncle
+ *
+ * @node: tree Node
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	return (node->parent->parent->left == node->parent ?
+														node->parent->parent->right
+													   : node->parent->parent->left);
+}
