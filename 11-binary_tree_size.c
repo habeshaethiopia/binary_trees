3a346875823958d12e981213bf72960174af826c
Aadane <adanemoges6@gmail.com>
2023-08-31 13:01:44 +0300

    SIZE OF THE TREE

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_size - find tree size
+ *
+ * @tree: the root
+ * Return: size_t size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+	size = binary_tree_size(tree->left);
+	size = size + binary_tree_size(tree->right);
+	return (size + 1);
+}
