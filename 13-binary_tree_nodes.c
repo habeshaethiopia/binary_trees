818b23d843dfb504fd3d02505f79835ed757c02a
Aadane <adanemoges6@gmail.com>
2023-08-31 13:23:45 +0300

    nodes with the child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - no of nodes wout the leaves
+ * binary_tree_nodes - no of nodes with atleast achild
  *
  * @tree: the root
  * Return: size_t size
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t size, leaves, nodes;
+	size_t nodes;
 
 	if (!tree)
 		return (0);
-	size = binary_tree_size(tree);
-	leaves = binary_tree_leaves(tree);
-	nodes = size - leaves;
-	return (nodes);
+	nodes = binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (!(!tree->left && !tree->right) ? nodes + 1 : nodes);
 }
