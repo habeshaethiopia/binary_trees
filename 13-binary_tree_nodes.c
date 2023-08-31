16012506545bacf471135931ec8f71f4a11edbb9
Aadane <adanemoges6@gmail.com>
2023-08-31 13:20:19 +0300

    trying

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,1 +1,18 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_nodes - no of nodes wout the leaves
+ *
+ * @tree: the root
+ * Return: size_t size
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t size, leaves, nodes;
+
+	if (!tree)
+		return (0);
+	size = binary_tree_size(tree);
+	leaves = binary_tree_leaves(tree);
+	nodes = size - leaves;
+	return (nodes);
+}
