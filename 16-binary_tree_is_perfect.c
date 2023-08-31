4ecdeaed121dcdbd82500b97ae44946c87684e0a
Aadane <adanemoges6@gmail.com>
2023-08-31 14:51:28 +0300

    check if perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 /**
+ * height - fund size of the tree
+ *
+ * @tree: tree root pointer
+ * Return: int size of the tree
+ */
+int height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	right = height(tree->right);
+	left = height(tree->left);
+	return (left > right ? left + 1 : right + 1);
+}
+/**
  * binary_tree_is_perfect - check if it is perfect or not
  *
  * @tree: the root
@@ -7,15 +23,22 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t left, right;
+
 	if (!tree)
 		return (0);
+	left = height(tree->left);
+	right = height(tree->right);
+	if (left == right)
+	{
+		if (!tree->left && !tree->right)
+			return (1);
 
-	if (!tree->left && !tree->right)
-		return (1);
+		if (!tree->left || !tree->right)
+			return (0);
 
-	if (!tree->left || !tree->right)
-		return (0);
-
-	return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
+	return (0);
 }
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,1 +1,11 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_sibling - find sibling
+ *
+ * @node: atree Node
+ * Return:binary_tree_t*
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    
+}
