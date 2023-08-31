e46876df5bf403ec437a9017c836eab3dd47ae6b
Aadane <adanemoges6@gmail.com>
2023-08-31 15:05:14 +0300

    ! missing

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,7 +7,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || node->parent)
+	if (!node || !node->parent)
 		return (NULL);
 	return (node->parent->left == node ? node->parent->right
 									   : node->parent->left);
