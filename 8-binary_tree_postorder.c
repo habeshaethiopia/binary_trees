d35cea28ff57fc784da6e1387c2d0b7097a9a342
Aadane <adanemoges6@gmail.com>
2023-08-31 11:55:37 +0300

    traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,1 +1,16 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - traversal function
+ *
+ * @tree:the variables
+ * @func:the variables
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	func(tree->n);
+	binary_tree_postorder(tree->right, func);
+}
