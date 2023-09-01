ddac69585ccc2a202fc73dc925193b32a6c91953
Aadane <adanemoges6@gmail.com>
2023-09-01 05:43:24 +0300

    right rotation

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,1 +1,26 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_rotate_right - funct
+ *
+ * @tree: root
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *pivot;
+
+	if (!tree || !tree->left)
+		return (tree);
+
+	pivot = tree->left;
+	tree->left = pivot->right;
+
+	if (pivot->right)
+		pivot->right->parent = tree;
+
+	pivot->right = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+
+	return (pivot);
+}
