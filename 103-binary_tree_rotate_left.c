b9caa609e75402f32e7080ce0ae09c1b4ded468a
Aadane <adanemoges6@gmail.com>
2023-09-01 05:38:24 +0300

    task 4

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,10 +1,25 @@
 #include "binary_trees.h"
 /**
  * binary_tree_rotate_left - rotete the tree
- * @tree: the root 
+ * @tree: the root
  * Return: binary_tree_t*
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *pivot;
 
+	if (!tree || !tree->right)
+		return (tree);
+
+	pivot = tree->right;
+	tree->right = pivot->left;
+
+	if (pivot->left)
+		pivot->left->parent = tree;
+
+	pivot->left = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+
+	return (pivot);
 }
