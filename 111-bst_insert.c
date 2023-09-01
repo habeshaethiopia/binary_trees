8851d27eafb762c642a4283c6ebc74f4d9859752
Aadane <adanemoges6@gmail.com>
2023-09-01 06:00:07 +0300

    insert ti bst

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -8,27 +8,4 @@
  */
 bst_t *bst_insert(bst_t **tree, int n)
 {
-	if (*tree == NULL)
-	{
-		*tree = (bst_t *)malloc(sizeof(bst_t));
-		if (*tree == NULL)
-		{
-			return (NULL);
-		}
-		(*tree)->n = n;
-		(*tree)->left = NULL;
-		(*tree)->right = NULL;
-		return (*tree);
-	}
-
-	if (n < (*tree)->n)
-	{
-		return (bst_insert(&(*tree)->left, n));
-	}
-	else if (n > (*tree)->n)
-	{
-		return (bst_insert(&(*tree)->right, n));
-	}
-
-	return (*tree);
 }
