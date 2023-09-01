ab4b455e82fb641a9f830445e63735d08bce0f89
Aadane <adanemoges6@gmail.com>
2023-09-01 05:57:08 +0300

    insert ti bst

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,1 +1,11 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_is_bst - function
+ *
+ * @tree: root
+ * Return: int
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+
+}
--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,1 +1,34 @@
 #include "binary_trees.h"
+/**
+ * bst_insert - func
+ *
+ * @tree: parameter
+ * @value: parameter
+ * Return: bst_t*
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (*tree == NULL)
+	{
+		*tree = (bst_t *)malloc(sizeof(bst_t));
+		if (*tree == NULL)
+		{
+			return (NULL);
+		}
+		(*tree)->value = value;
+		(*tree)->left = NULL;
+		(*tree)->right = NULL;
+		return (*tree);
+	}
+
+	if (value < (*tree)->value)
+	{
+		return (bst_insert(&(*tree)->left, value));
+	}
+	else if (value > (*tree)->value)
+	{
+		return (bst_insert(&(*tree)->right, value));
+	}
+
+	return (*tree);
+}
