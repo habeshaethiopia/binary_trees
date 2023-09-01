5cea895abd68e5b6ff2861109fea3b49b7e6bff4
Aadane <adanemoges6@gmail.com>
2023-09-01 03:47:25 +0300

    advance 1

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_trees_ancestor(
 	const binary_tree_t *first, const binary_tree_t *second)
 {
-	binary_tree_t *temp;
+	const binary_tree_t *temp;
 
 	if (!first || !second)
 		return (NULL);
