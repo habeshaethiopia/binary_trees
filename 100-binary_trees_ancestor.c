e98c8ef403d234e2429b37740f795690ac6abd6d
Aadane <adanemoges6@gmail.com>
2023-09-01 04:04:04 +0300

    advance 1

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,20 +9,21 @@
 binary_tree_t *binary_trees_ancestor(
 	const binary_tree_t *first, const binary_tree_t *second)
 {
-	binary_tree_t *temp;
+	binary_tree_t *temp, *fi;
 
 	if (!first || !second)
 		return (NULL);
-	while (first)
+	fi = (binary_tree_t *)first;
+	while (fi)
 	{
 		temp = (binary_tree_t *)second;
-		while (second)
+		while (temp)
 		{
-			if (first->parent == temp->parent)
-				return (first->parent);
+			if (fi == temp)
+				return (fi);
 			temp = temp->parent;
 		}
-		first = first->parent;
+		fi = fi->parent;
 	}
 	return (NULL);
 }
