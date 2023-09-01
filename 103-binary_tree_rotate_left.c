aae68045d630f80734dd273b54b4548326767fda
Aadane <adanemoges6@gmail.com>
2023-09-01 05:23:14 +0300

    advance 3

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,1 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_rotate_left - rotete the tree
+ * @tree: the root 
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+
+}
